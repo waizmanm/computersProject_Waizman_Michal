890ed48f22e88561eb0ef7b7fb75042131680b99
waizmanm <47328652+waizmanm@users.noreply.github.com>
2019-02-05 01:17:03 +0200

    Update main.py
    final

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import math
 import matplotlib.pyplot as plt
 
-
-
-#data_path = 'C:/work/fitgui/example-lin.txt'
-#data_path = 'C:\work\inputOutputExamples\workingCols\input.txt'
-#data_path = 'c:\work\inputOutputExamples\workingRows\input.txt'
+# data_path = 'C:/work/fitgui/example-lin.txt'
+# data_path = 'C:\work\inputOutputExamples\workingCols\input.txt'
+# data_path = 'c:\work\inputOutputExamples\workingRows\input.txt'
 data_path = input("Please insert file path:")
-#check rows
+
+
+# check rows
 def check_rows(splt_lines):
     for i in range(1, len(splt_lines)):
         if len(splt_lines[0]) != len(splt_lines[i]):
@@ -15,13 +15,15 @@ def check_rows(splt_lines):
 
     return []
 
-#transpose
+
+# transpose
 def transpose(splt_lines):
     for row in splt_lines:
         splt_lines = [[splt_lines[j][i] for j in range(len(splt_lines))] for i in range(len(splt_lines[0]))]
         return splt_lines
 
-#converts string to float
+
+# converts string to float
 def str2float(s):
     try:
         v = float(s)
@@ -38,23 +40,24 @@ def strlist2floatlis(ss):
         ff.append(str2float(item))
     return ff
 
+
 # check data validity
 def isvalid_data_columns(splt_lines):
-
     for line in splt_lines[1:]:
         vv = strlist2floatlis(line)
-        if len(vv)!=4 :
+        if len(vv) != 4:
             return 'Input file error: Data lists are not the same length.'
-        elif math.nan  in vv:
-            return  'Input file error: Data contains not-a-numbers.'
+        elif math.nan in vv:
+            return 'Input file error: Data contains not-a-numbers.'
 
     return []
 
+
 # forms data matrix for linfit
 def setdata_columns(splt_lines):
-    xydxdy=[[],[],[],[]]
+    xydxdy = [[], [], [], []]
 
-    x_ind=splt_lines[0].index('x')
+    x_ind = splt_lines[0].index('x')
     dx_ind = splt_lines[0].index('dx')
     y_ind = splt_lines[0].index('y')
     dy_ind = splt_lines[0].index('dy')
@@ -66,34 +69,36 @@ def setdata_columns(splt_lines):
         xydxdy[2].append(vv[dx_ind])
         xydxdy[3].append(vv[dy_ind])
 
+    return xydxdy
 
 
-    return  xydxdy
-
-#checks   uncertainties
+# checks   uncertainties
 def all_uncertainties_positive(vv):
-    all_positve = sum(1 for v in vv if v <0)==0
+    all_positve = sum(1 for v in vv if v < 0) == 0
     return all_positve
 
-#dot product for two lists
-#assuming same length according to
-#checking data on input
-def dot_list(x,y):
-    ret= 0
+
+# dot product for two lists
+# assuming same length according to
+# checking data on input
+def dot_list(x, y):
+    ret = 0
     for index, x_item in enumerate(x):
-        ret+= x_item*y[index]
+        ret += x_item * y[index]
     return ret
-#chi
-def chi_sqr(x,y,a,b,dy):
+
+
+# chi
+def chi_sqr(x, y, a, b, dy):
     v = []
     for index, x_item in enumerate(x):
-        v.append ((y[index] - a*x_item - b)/dy[index])
+        v.append((y[index] - a * x_item - b) / dy[index])
     vv = [(v1 * v1) for v1 in v]
     return sum(vv)
 
-#main function
-def fitlin(filename):
 
+# main function
+def fitlin(filename):
     try:
         with open(filename) as f:
             lines = f.readlines()
@@ -102,37 +107,34 @@ def fitlin(filename):
         print('fails opening input file')
         return
 
-    splt_lines=[]
+    splt_lines0 = []
     for line in lines:
-        splt_lines.append(line.strip().lower().split())
+        splt_lines0.append(line.strip().lower().split())
 
-    splt_lines =splt_lines [0:splt_lines.index([])]
-
-    if len(splt_lines) == 4: # row style data
+    splt_lines = splt_lines0[0:splt_lines0.index([])]
+    abrv_lines= splt_lines0[1+splt_lines0.index([]):]
+    if len(splt_lines) == 4:  # row style data
         errMsg = check_rows(splt_lines)
-        if(errMsg!=[]):
+        if (errMsg != []):
             print(errMsg)
             return
 
-        splt_lines=transpose(splt_lines)
+        splt_lines = transpose(splt_lines)
 
-
-    xydxdy_check = set(sorted(splt_lines[0])) & set(sorted(['x','y','dx','dy']))
+    xydxdy_check = set(sorted(splt_lines[0])) & set(sorted(['x', 'y', 'dx', 'dy']))
     if len(xydxdy_check) != 4:
         print('data abreviations are different from agreed "x","y","dx","dy" ')
         return
 
     errMessage = isvalid_data_columns(splt_lines)
-    if errMessage !=[]:
+    if errMessage != []:
         print(errMessage)
         return
     else:
         xydxdy = setdata_columns(splt_lines)
 
-
-
-    if all_uncertainties_positive(xydxdy[2]+xydxdy[3])==False :
-        print( 'Input file error: Not all uncertainties are positive.')
+    if all_uncertainties_positive(xydxdy[2] + xydxdy[3]) == False:
+        print('Input file error: Not all uncertainties are positive.')
         return
 
     x = xydxdy[0]
@@ -140,42 +142,59 @@ def fitlin(filename):
     dx = xydxdy[2]
     dy = xydxdy[3]
 
-    one_over_dydy = [1.0/(v * v) for v in dy]
+    one_over_dydy = [1.0 / (v * v) for v in dy]
     sum_one_over_dydy = sum(one_over_dydy)
-    x_tag = dot_list(x,one_over_dydy)/sum_one_over_dydy
-    y_tag = dot_list(y,one_over_dydy)/sum_one_over_dydy
+    x_tag = dot_list(x, one_over_dydy) / sum_one_over_dydy
+    y_tag = dot_list(y, one_over_dydy) / sum_one_over_dydy
 
-    xy = [a*b for a,b in zip(x, y)]
-    xy_tag = dot_list(xy,one_over_dydy)/sum_one_over_dydy
+    xy = [a * b for a, b in zip(x, y)]
+    xy_tag = dot_list(xy, one_over_dydy) / sum_one_over_dydy
     xx = [a * b for a, b in zip(x, x)]
-    xx_tag = dot_list(xx,one_over_dydy)/sum_one_over_dydy
+    xx_tag = dot_list(xx, one_over_dydy) / sum_one_over_dydy
 
-    DEN = (xx_tag - x_tag*x_tag)
-    a = (xy_tag - x_tag*y_tag)/DEN
-    b = y_tag - a*x_tag
+    DEN = (xx_tag - x_tag * x_tag)
+    a = (xy_tag - x_tag * y_tag) / DEN
+    b = y_tag - a * x_tag
 
-    dydy = [a*b for a,b in zip(dy, dy)]
-    dydy_tag = dot_list(dydy,one_over_dydy)/sum_one_over_dydy
+    dydy = [a * b for a, b in zip(dy, dy)]
+    dydy_tag = dot_list(dydy, one_over_dydy) / sum_one_over_dydy
 
     N = len(x)
-    da = math.sqrt(dydy_tag/(N*DEN))
-    db = math.sqrt(dydy_tag*xx_tag/(N*DEN))
+    da = math.sqrt(dydy_tag / (N * DEN))
+    db = math.sqrt(dydy_tag * xx_tag / (N * DEN))
 
+    chichi = chi_sqr(x, y, a, b, dy)
+    chichi_red = chichi / (N - 2)
 
-    chichi = chi_sqr(x,y,a,b,dy)
-    chichi_red = chichi/(N-2)
-
-
-    print("a = {0:17.15f} +- {1:19.17f}".format(a,da))
-    print("b = {0:17.15f} +- {1:19.17f}".format(b,db))
+    print("a = {0:17.15f} +- {1:19.17f}".format(a, da))
+    print("b = {0:17.15f} +- {1:19.17f}".format(b, db))
     print("chi2 = {0:17.15f}".format(chichi))
     print("chi2_reduced = {0:17.15f}".format(chichi_red))
 
-    plt.plot(x, y, 'r+')
+    xlable = "x axis"
+    if len(abrv_lines) > 0:
+        xlable = "".join(abrv_lines[0][2:])
+    ylable = "y axis"
+    if len(abrv_lines) > 1:
+        ylable = "".join(abrv_lines[1][2:])
+
+    x_lefts = [x_value - dx[index]  for index, x_value in enumerate(x)]
+    x_right = [x_value + dx[index]  for index, x_value in enumerate(x)]
+    y_up    = [y_value - dy[index]  for index, y_value in enumerate(y)]
+    y_lo    = [y_value + dy[index]  for index, y_value in enumerate(y)]
+
+    plt.plot(x, y, 'r.')
+    plt.xlabel(xlable)
+    plt.ylabel(ylable)
+    # plt.title('Histogram of IQ')
+    for ii in range(0,len(x)):
+        plt.plot([x_lefts[ii], x_right[ii]], [y[ii]   , y[ii]   ], 'b')
+        plt.plot([x[ii]      , x[ii]      ], [y_up[ii], y_lo[ii]], 'b')
+
+    line_xx = [ min(x), max(x)]
+    line_yy = [a*v + b for v in line_xx]
+
+    plt.plot(line_xx, line_yy, 'r-')
     plt.show()
 
-
-
-
-     
 fitlin(data_path)
