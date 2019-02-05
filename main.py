0e0c727ee1cb1f248538361139967e07111517ba
waizmanm <47328652+waizmanm@users.noreply.github.com>
2019-02-05 19:36:59 +0200

    Update main.py
    most final

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import math
 import matplotlib.pyplot as plt
 
-# data_path = 'C:/work/fitgui/example-lin.txt'
+
+#  DEBUGGING
 # data_path = 'C:\work\inputOutputExamples\workingCols\input.txt'
 # data_path = 'c:\work\inputOutputExamples\workingRows\input.txt'
 data_path = input("Please insert file path:")
 
 
-# check rows
+# checks equality of substring number for all lines
 def check_rows(splt_lines):
     for i in range(1, len(splt_lines)):
         if len(splt_lines[0]) != len(splt_lines[i]):
@@ -16,7 +17,9 @@ def check_rows(splt_lines):
     return []
 
 
-# transpose
+# transpose string matrix
+# splt_lines are presumed having same number of substrings
+# according for previous validity checks
 def transpose(splt_lines):
     for row in splt_lines:
         splt_lines = [[splt_lines[j][i] for j in range(len(splt_lines))] for i in range(len(splt_lines[0]))]
@@ -41,7 +44,7 @@ def strlist2floatlis(ss):
     return ff
 
 
-# check data validity
+# check data validity for cast to float
 def isvalid_data_columns(splt_lines):
     for line in splt_lines[1:]:
         vv = strlist2floatlis(line)
@@ -54,6 +57,7 @@ def isvalid_data_columns(splt_lines):
 
 
 # forms data matrix for linfit
+# i.e. cast strings to proper types
 def setdata_columns(splt_lines):
     xydxdy = [[], [], [], []]
 
@@ -88,7 +92,7 @@ def dot_list(x, y):
     return ret
 
 
-# chi
+# chi^2 computation for ax+b = y wrt. uncertainties dy
 def chi_sqr(x, y, a, b, dy):
     v = []
     for index, x_item in enumerate(x):
@@ -111,27 +115,30 @@ def fitlin(filename):
     for line in lines:
         splt_lines0.append(line.strip().lower().split())
 
-    splt_lines = splt_lines0[0:splt_lines0.index([])]
-    abrv_lines= splt_lines0[1+splt_lines0.index([]):]
-    if len(splt_lines) == 4:  # row style data
-        errMsg = check_rows(splt_lines)
+    #data for chi computation
+    data_lines = splt_lines0[0:splt_lines0.index([])]
+    #axis lables
+    abrv_lines = lines[1+splt_lines0.index([]):]
+    if len(data_lines) == 4:  # row style data
+        errMsg = check_rows(data_lines)
         if (errMsg != []):
             print(errMsg)
             return
+        #transpose of 'strings' matrix - candidates for transforming to actual data
+        data_lines = transpose(data_lines)
 
-        splt_lines = transpose(splt_lines)
-
-    xydxdy_check = set(sorted(splt_lines[0])) & set(sorted(['x', 'y', 'dx', 'dy']))
+    #check of validity for variables abbreviation string
+    xydxdy_check = set(sorted(data_lines[0])) & set(sorted(['x', 'y', 'dx', 'dy']))
     if len(xydxdy_check) != 4:
         print('data abreviations are different from agreed "x","y","dx","dy" ')
         return
 
-    errMessage = isvalid_data_columns(splt_lines)
+    errMessage = isvalid_data_columns(data_lines)
     if errMessage != []:
         print(errMessage)
         return
     else:
-        xydxdy = setdata_columns(splt_lines)
+        xydxdy = setdata_columns(data_lines)
 
     if all_uncertainties_positive(xydxdy[2] + xydxdy[3]) == False:
         print('Input file error: Not all uncertainties are positive.')
@@ -142,6 +149,7 @@ def fitlin(filename):
     dx = xydxdy[2]
     dy = xydxdy[3]
 
+    #linfit computation
     one_over_dydy = [1.0 / (v * v) for v in dy]
     sum_one_over_dydy = sum(one_over_dydy)
     x_tag = dot_list(x, one_over_dydy) / sum_one_over_dydy
@@ -152,7 +160,7 @@ def fitlin(filename):
     xx = [a * b for a, b in zip(x, x)]
     xx_tag = dot_list(xx, one_over_dydy) / sum_one_over_dydy
 
-    DEN = (xx_tag - x_tag * x_tag)
+    DEN = (xx_tag - x_tag * x_tag) # denominator
     a = (xy_tag - x_tag * y_tag) / DEN
     b = y_tag - a * x_tag
 
@@ -171,12 +179,22 @@ def fitlin(filename):
     print("chi2 = {0:17.15f}".format(chichi))
     print("chi2_reduced = {0:17.15f}".format(chichi_red))
 
+    #lable strings interpretation:
+    #substring before ':' is key for substring after it
+    id_lable = {}
+    for line in abrv_lines:
+        ss = line.split(':')
+        if len(ss)>1:
+            id_lable[ss[0].lower()] = ss[1]
+        #otherwise such a strin is out of <key>:<value> convention
+
+
     xlable = "x axis"
-    if len(abrv_lines) > 0:
-        xlable = "".join(abrv_lines[0][2:])
+    if id_lable.get(xlable) != None:
+       xlable = id_lable[xlable].strip()
     ylable = "y axis"
-    if len(abrv_lines) > 1:
-        ylable = "".join(abrv_lines[1][2:])
+    if id_lable.get(ylable) != None:
+        ylable = id_lable[ylable].strip()
 
     x_lefts = [x_value - dx[index]  for index, x_value in enumerate(x)]
     x_right = [x_value + dx[index]  for index, x_value in enumerate(x)]
